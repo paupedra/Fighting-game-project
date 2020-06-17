b5269551a827e831ad43e985ed9dd91c55130c19
Serfin01 <sergilozano96@gmail.com>
2020-06-17 17:22:06 +0200

    salto y movimiento done

---
+++ Assets/Scripts/MovementChar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementChar : MonoBehaviour
+{
+    public CharController controller;
+
+    public float runSpeed = 0f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
