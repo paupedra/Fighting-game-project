660e96e9bb40d42f85dbe06486eecee6ef748032
Serfin01 <sergilozano96@gmail.com>
2020-06-18 02:52:50 +0200

    se mueve, salta, y animaciones de movimiento vertical(mas o menos) y horizontal

--- Assets/Scripts/MovementChar.cs
+++ Assets/Scripts/MovementChar.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class MovementChar : MonoBehaviour
 {
     public CharController controller;
+    public Animator animator;
 
     public float runSpeed = 0f;
     float horizontalMove = 0f;
     bool jump = false;
     bool crouch = false;
+    public Rigidbody2D rigidbody2D;
 
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+
+        UpdateAnimations();
 
         if (Input.GetButtonDown("Jump"))
         {
@@ -31,5 +36,12 @@ public class MovementChar : MonoBehaviour
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
         crouch = false;
+    }
+
+    void UpdateAnimations()
+    {
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+        animator.SetFloat("velocityY", rigidbody2D.velocity.y);
+        animator.SetBool("isGrounded", controller.m_Grounded);
     }
 }
