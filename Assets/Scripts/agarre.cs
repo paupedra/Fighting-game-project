36834fb39894fb46713f2b1cb9e6e40b6520c87d
Serfin01 <sergilozano96@gmail.com>
2020-06-26 14:24:06 +0200

    se agarra casi

--- Assets/Scripts/agarre.cs
+++ Assets/Scripts/agarre.cs
@@ -1,19 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//falta crear bool script movement para si salta o no el marico
 public class agarre : MonoBehaviour
 {
-    public Rigidbody2D rg; 
+    public Rigidbody2D rg;
+    bool puedeSalir = true;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        SalirAgarre();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="borde")
+        rg.isKinematic = true;
+        puedeSalir = false;
+        if(puedeSalir == false)
+        {
+            rg.constraints = RigidbodyConstraints2D.FreezePositionY;
+            rg.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "borde")
+            rg.isKinematic = false;
         
+    }
+    private void SalirAgarre()
+    {
+        if(Input.GetButtonDown("Horizontal") || Input.GetKeyDown(KeyCode.Space))
+        {
+            puedeSalir = true;
+            rg.constraints = RigidbodyConstraints2D.None;
+            rg.constraints = RigidbodyConstraints2D.FreezeRotation;
+            rg.isKinematic = false;
+        }
     }
 }
